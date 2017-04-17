a8f22f62081b520f266f03ba566fe3c268e45739
Jorge Macias <jmacias9564@gmail.com>
2017-04-16 19:52:37 -0500

    Update server_functions.c

--- server_functions.c
+++ server_functions.c
@@ -62,30 +62,15 @@ void close_serv(struct serv *server) {
 	free(server);
 }
 
-int tcp_proc(int argc, char *argv[]) //tcp socket process: Jorge Macias
+int tcp_proc(struct serv *server) //tcp socket process: Jorge Macias
 {
-	int sockfd, newsockfd, portno, procid;
+	int sockfd, newsockfd, portno, pid;
 	socklen_t clilen;
 	struct sockaddr_in serv_addr, cli_addr;
-	if (argc < 2) {
-		fprintf(stderr,"ERROR, no port provided\n");
-		exit(1);
-	}
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) 
-		error("ERROR opening socket");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = atoi(argv[1]);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
-	if (bind(sockfd, (struct sockaddr *) &serv_addr,
-				sizeof(serv_addr)) < 0) 
-		error("ERROR on binding");
-	listen(sockfd,5);
+	
 	clilen = sizeof(cli_addr);
 	//implement while loop: Jorge Macias
-	while (1)
+	while (true)
 	{
 		newsockfd = accept(sockfd, 
 				(struct sockaddr *) &cli_addr, &clilen);
@@ -94,10 +79,10 @@ int tcp_proc(int argc, char *argv[]) //tcp socket process: Jorge Macias
 		//zombie process signal handler: Jorge Macias
 		signal(SIGCHLD,SIG_IGN);
 		//implement fork call: Jorge Macias
-		procid = fork();
-		if (procid < 0) 
+		pid = fork();
+		if (pid < 0) 
 			error("ERROR on fork");
-		if (procid == 0)
+		if (pid == 0)
 		{
 			close (sockfd);
 			tcp_comm(newsockfd); //call tcp_comm function: Jorge Macias
